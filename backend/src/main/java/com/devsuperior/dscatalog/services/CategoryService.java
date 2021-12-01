a05fbdfb625a9a015c59bb5e6b07fd4c57409eb7
NaianaCLima <naianacordeirodelima@gmail.com>
2021-12-01 14:37:19 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired//injeta automaticamente uma instância gerenciada pelo spring
 	private CategoryRepository repository;//acessa o repository e chama as categorias do BD
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() { //findAll é o nome do método, pode ser qlq um.
 		return repository.findAll();//acessa o repository e chama as categorias do BD, coloca na interface(classe) o @Repository
 	}
