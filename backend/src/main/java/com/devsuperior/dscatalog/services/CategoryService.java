8a87a200eabaa65ad78edc699f1801f9d3c58879
NaianaCLima <naianacordeirodelima@gmail.com>
2021-12-03 11:18:01 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -33,4 +33,11 @@ public class CategoryService {
 		
 	}
 
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);//retornando a entidade para CategoryDTO
+	}
+
 }
