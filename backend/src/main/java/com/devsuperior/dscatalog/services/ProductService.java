c92ee6fa13a92dd0d7870bf196436e62fb6cc86a
NaianaCLima <naianacordeirodelima@gmail.com>
2021-12-09 14:15:46 -0300

    Product insert, update, delete

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -23,17 +26,21 @@ public class ProductService {
 
 	@Autowired // injeta automaticamente uma instância gerenciada pelo spring
 	private ProductRepository repository;// acessa o repository e chama as categorias do BD
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) { // findAll é o nome do método, pode ser qlq um.
-		Page<Product> list = repository.findAll(pageRequest);// acessa o repository e chama as categorias do BD, coloca na													
+		Page<Product> list = repository.findAll(pageRequest);// acessa o repository e chama as categorias do BD, coloca
+																// na
 		return list.map(x -> new ProductDTO(x));
 	}
 
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado!"));																											
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado!"));
 		return new ProductDTO(entity, entity.getCategories());
 
 	}
@@ -41,7 +48,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);// retornando a entidade para ProductDTO
 	}
@@ -50,10 +57,10 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getOne(id);// se chamar o getOne e o ID n existir o try catch trata a excessão
-			//entity.setName(dto.getName());
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
-		} catch (EntityNotFoundException e) {//exceção específica 
+		} catch (EntityNotFoundException e) {// exceção específica
 			throw new ResourceNotFoundException("Id não existe! " + id);
 
 		}
@@ -63,13 +70,32 @@ public class ProductService {
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-			
-		} catch (EmptyResultDataAccessException e) {//tratando exceção caso tenha um id inexistente
-			throw new ResourceNotFoundException("Id não existe! " + id);//classe criada por nós
 
-		}catch(DataIntegrityViolationException e) {//para garantir a integridade do BD (Não deletar o que não pode ser deletado)
+		} catch (EmptyResultDataAccessException e) {// tratando exceção caso tenha um id inexistente
+			throw new ResourceNotFoundException("Id não existe! " + id);// classe criada por nós
+
+		} catch (DataIntegrityViolationException e) {// para garantir a integridade do BD (Não deletar o que não pode
+														// ser deletado)
 			throw new DatabaseException("Violação de integridade!");
 		}
+
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());//produto
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();//limpa se tiver carregado
+		for(CategoryDTO catDTO : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDTO.getId());
+			entity.getCategories().add(category);
+			
+		}
+		
+		
 
 	}
 
